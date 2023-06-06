2449fc4c4edf4d33548f735898ff8951ff505b42
nazzy-tech <58662791+nazzy-tech@users.noreply.github.com>
2023-06-06 13:17:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+const toggle = document.getElementById("toggleDark");
+const body = document.querySelector("body");
 
+toggle.addEventListener('click', function(){
+    this.classList.toggle('fa-moon');
+    if(this.classList.toggle('fa-sharp fa-solid fa-moon')){
+        body.styl.background = 'white';
+        body.style.color = 'black';
+        body.style.transition = '2s';
+    }else{
+        body.styl.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '2s';
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+// let Icon = document.getElementById("Icon");
+// 
+// Icon.onclick = function(){
+    // document.body.classList.toggle("dark-theme");
+    // if(document.body.classList.contains("dark-theme")){
+        // moonIcon.src = "./img/moon-removebg-preview (1).png"
+    // }
+// }
+// 
